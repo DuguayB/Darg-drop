afc04a3187dcd1216415467edebd7e080cf84c39
Cédric Cadet <Cdric Cadet>
2016-05-18 15:38:35 +0200

    boite a outil debut

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+$(document).ready(function() {
 
-$(document).ready(function() { 
+	var graph_tools = new joint.dia.Graph;
+
+    var paper_tools = new joint.dia.Paper({
+    	el: $('#tools'),
+    	width: 220,
+    	height: 630,
+    	model: graph,
+    	gridSize: 1,
+    });
+
+    var rect_tools = new joint.shapes.basic.Rect({
+        position: { x: 0, y: 30 },
+        size: { width: 100, height: 30 },
+        attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+    });
+
+	var cercle_tools = new joint.shapes.basic.Circle({
+    	position : {x: 0, y: 90},
+    	size: { width: 50, height: 50}
+    });
+
+    var rect2_tools = new joint.shapes.basic.Rect({
+        position: { x: 0, y: 120 },
+        size: { width: 100, height: 30 },
+        attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+    });
+
+	graph_tools.addCells([rect_tools, cercle_tools, rect2_tools]);
+
+
    var graph = new joint.dia.Graph;
 
    /* CANEVAS */
@@ -8,8 +38,10 @@ $(document).ready(function() {
         width: 1000,
         height: 500,
         model: graph,
-        gridSize: 1
+        gridSize: 1,
     });
+
+
 
     /* CREER UN ITEM */
     var rect = new joint.shapes.basic.Rect({
@@ -19,9 +51,9 @@ $(document).ready(function() {
     });
 
     var cercle = new joint.shapes.basic.Circle({
-    	position : {x: 100, y: 250},
-    	size: { width: 100, height: 100}
-    })
+    	position : {x: 100, y: 90},
+    	size: { width: 50, height: 50}
+    });
 
     var rect2 = rect.clone();
     rect2.translate(300);
