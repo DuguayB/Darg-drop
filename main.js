a575f0a9ea30fdcc4738e262e20daae532bae79d
Cédric Cadet <Cdric Cadet>
2016-05-19 11:33:01 +0200

    commit n°1

--- main.js
+++ main.js
@@ -1,71 +1,57 @@
+
+
 $(document).ready(function() {
 
 	var graph_tools = new joint.dia.Graph;
 
-    var paper_tools = new joint.dia.Paper({
-    	el: $('#tools'),
-    	width: 220,
-    	height: 630,
-    	model: graph,
-    	gridSize: 1,
-    });
-
-    var rect_tools = new joint.shapes.basic.Rect({
-        position: { x: 0, y: 30 },
-        size: { width: 100, height: 30 },
-        attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
-    });
-
-	var cercle_tools = new joint.shapes.basic.Circle({
-    	position : {x: 0, y: 90},
-    	size: { width: 50, height: 50}
-    });
-
-    var rect2_tools = new joint.shapes.basic.Rect({
-        position: { x: 0, y: 120 },
-        size: { width: 100, height: 30 },
-        attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
-    });
-
-	graph_tools.addCells([rect_tools, cercle_tools, rect2_tools]);
-
-
-   var graph = new joint.dia.Graph;
-
-   /* CANEVAS */
-    var paper = new joint.dia.Paper({
-        el: $('#myholder'),
-        width: 1000,
-        height: 500,
-        model: graph,
+	var paper_tools = new joint.dia.Paper({
+		el: $('#tools'),
+		width: $('#tools').width(),
+        height: $('#tools').height(),
+        model: graph_tools,
         gridSize: 1,
-    });
+        interactive: false
+	})
 
+	var image = new joint.shapes.basic.Image({
+    	position : {x : 100, y : 20 },
+    	size : { width : 50, height : 50 },
+    	attrs : { image : { "xlink:href" : "thalie_9843R1w1.jpg", width : 50, height : 50 } }
+	});
 
+	var image2 = new joint.shapes.basic.Image({
+    	position : {x : 100, y : 90 },
+    	size : { width : 50, height : 50 },
+    	attrs : { image : { "xlink:href" : "5655c3ff.jpg", width : 50, height : 50 } }
+	});
+	
+	var image3 = new joint.shapes.basic.Image({
+    	position : {x : 100, y : 160 },
+    	size : { width : 50, height : 50 },
+    	attrs : { image : { "xlink:href" : "Buttocksurgery.jpg", width : 50, height : 50 } }
+	});
+	
+	var image4 = new joint.shapes.basic.Image({
+    	position : {x : 100, y : 230 },
+    	size : { width : 50, height : 50 },
+    	attrs : { image : { "xlink:href" : "d5302569.jpg", width : 50, height : 50 } }
+	});
 
-    /* CREER UN ITEM */
-    var rect = new joint.shapes.basic.Rect({
-        position: { x: 100, y: 30 },
-        size: { width: 100, height: 30 },
-        attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
-    });
+	var image5 = new joint.shapes.basic.Image({
+    	position : {x : 100, y : 300 },
+    	size : { width : 50, height : 50 },
+    	attrs : { image : { "xlink:href" : "LOL.jpg", width : 50, height : 50 } }
+	});
 
-    var cercle = new joint.shapes.basic.Circle({
-    	position : {x: 100, y: 90},
-    	size: { width: 50, height: 50}
-    });
+	graph_tools.addCells([image, image2, image3, image4, image5]);
 
-    var rect2 = rect.clone();
-    rect2.translate(300);
+	var graph_tools = new joint.dia.Graph;
 
-    /* CREER UN LIEN ENTRE DEUX ITEMS */
-    var link = new joint.dia.Link({
-        source: { id: rect.id },
-        target: { id: rect2.id }
-    });
-
-    /* CREER UNE COPIE D'UN ITEM */
-    var rect3 = rect.clone();
-
-    graph.addCells([rect, rect2, link, rect3, cercle]);
+	var paper = new joint.dia.paper({
+		el: $('#myholder'),
+		width: $('#myholder').width(),
+		height: ($('#myholder').height() - 50),
+		model: graph
+	})
 });
+
