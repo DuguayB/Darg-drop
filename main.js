84e7cae0e89e739bcf985d85cdf4a22ad656ba6b
Cédric Cadet <Cdric Cadet>
2016-05-19 11:48:04 +0200

    Base Projet 'fonctionnel'

--- main.js
+++ main.js
@@ -45,13 +45,47 @@ $(document).ready(function() {
 
 	graph_tools.addCells([image, image2, image3, image4, image5]);
 
-	var graph_tools = new joint.dia.Graph;
 
-	var paper = new joint.dia.paper({
+	var graph = new joint.dia.Graph;
+
+	var paper = new joint.dia.Paper({
 		el: $('#myholder'),
 		width: $('#myholder').width(),
-		height: ($('#myholder').height() - 50),
+		height: $('#myholder').height(),
 		model: graph
-	})
+	});
+
+	paper_tools.on('cell:pointerdown', function(cellView, e, x, y) {
+		$('body').append('<div id="flyPaper" style="position:fixed;z-index:100;opacity:.7;pointer-event:none;"></div>');
+		var flyGraph = new joint.dia.Graph,
+			flyPaper = new joint.dia.Paper({ 
+				el: $('#flyPaper'),
+				model: flyGraph,
+				interactive: false 
+			}), 
+			flyShape = cellView.model.clone(),
+			pos = cellView.model.position(),
+			offset = { x: x - pos.x, y: y - pos.y };
+		flyShape.position(0, 0);
+		flyGraph.addCell(flyShape);
+		$("#flyPaper").offset({ left: e.pageX - offset.x, top: e.pageY - offset.y });
+		$('body').on('mousemove.fly', function(e) {
+			$("#flyPaper").offset({ 
+				left: e.pageX - offset.x, top: e.pageY - offset.y 
+			});
+		}); 
+		$('body').on('mouseup.fly', function(e) {
+			var x = e.pageX, y = e.pageY, target = paper.$el.offset();
+			// Dropped over paper ?
+			if (x > target.left && x < target.left + paper.$el.width() && y > target.top && y < target.top + paper.$el.height()) {
+				var s = flyShape.clone();
+				s.position(x - target.left - offset.x, y - target.top - offset.y);
+				graph.addCell(s);
+			} 
+			$('body').off('mousemove.fly').off('mouseup.fly');
+			flyShape.remove();
+			$('#flyPaper').remove();
+		});
+	});
 });
 
